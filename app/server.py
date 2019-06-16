8f3823388c0577a550a3589824f4d09bef5b0838
aryanvaid <51590506+aryanvaid@users.noreply.github.com>
2019-06-16 11:49:20 +0530

    Update server.py

--- app/server.py
+++ app/server.py
@@ -9,7 +9,7 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://drive.google.com/uc?export=download&id=1j4hIv1nD0wlVvcyR3JXOke_Nt8vlJJtU'
+export_file_url = 'https://www.dropbox.com/s/a3p8rz8m8ief9dy/model-rajat.pkl?dl=1'
 export_file_name = 'export.pkl'
 
 classes = ['Pneumonia','Normal']
